6543b9650be8a84f1f3fbc98827ded1324d9151d
KennethSettens <kennysettens1234@gmail.com>
2023-08-28 16:35:54 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+from models import db, connect_db, Pet
+from forms import AddPetForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "1235"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+with app.app_context():
+    connect_db(app)
+    db.create_all()
+
+
+@app.route('/')
+def list_pets():
+    pets = Pet.query.all()
+    return render_template("pet_list.html", pets=pets)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    form = AddPetForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        url = form.url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name=name, species=species,
+                  url=url, age=age, notes=notes)
+        db.session.add(pet)
+        db.session.commit()
+        flash(f"{pet.name} added.")
+        return redirect(url_for('list_pets'))
+    else:
+        return render_template("pet_add_form.html", form=form)
+
+# @app.route("/<int:pet_id>", methods=["GET", "POST"])
+# def edit_pet(pet_id):
+#
+#
+# @app.route("/api/pets/<int:pet_id>", methods=['GET'])
+# def api_get_pet(pet_id):
+#     return render_template("base.html")
+#
+# if __name__ == '__main__':
+#     app.run()
---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
+
+
+class AddPetForm(FlaskForm):
+    name = StringField(
+        "Pet Name",
+        validators=[InputRequired()],
+    )
+    species = SelectField(
+        "Species",
+        choices=[("cat", "Cat"),
+                 ("dog", "Dog"),
+                 ("porcupine", "Porcupine")],
+    )
+    url = StringField(
+        "Image URL",
+        validators=[Optional(), URL()],
+    )
+    age = IntegerField(
+        "Age",
+        validators=[Optional(),
+                    NumberRange(min=0, max=30)],
+    )
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=5)],
+    )
+
+
